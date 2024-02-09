34fc8e919a7f0c7039b6c74769654439d752690b
Dean Phillips <dean.phillips@e-hps.com>
2024-02-08 19:30:13 -0600

    return error string from the run query tool

--- main.py
+++ main.py
@@ -15,22 +15,29 @@ chat = ChatOpenAI()
 prompt = ChatPromptTemplate(
     messages=[
         HumanMessagePromptTemplate.from_template("{input}"),
+        # Will look for an input with the name "agent_scratchpad".
+        # the agent scratchpad is basically just memory that remembers the initial human message,
+        # any AI responses, function call results, etc... that it can pass during subsequant requests
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
 tools = [run_query_tool]
 
+# Basically a chain with all the normal things like input and prompts but adds available
+# tools to the request that goes to OpenAI
 agent = OpenAIFunctionsAgent(
     llm=chat,
     prompt=prompt,
     tools=tools
 )
 
+# Takes a chain (in the form of an agent) and calls the OpenAI API over and over
+# until it gets a response that is not a request to call a tool.
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
     tools=tools
 )
 
-agent_executor("How many users are in the database")
+agent_executor("How many uses have added addresses?")
