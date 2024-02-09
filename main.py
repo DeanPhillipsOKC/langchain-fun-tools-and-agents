01689fbbfb60b9f75eddef7fc0f6628b8a905c98
Dean Phillips <dean.phillips@e-hps.com>
2024-02-09 16:11:30 -0600

    added memory so that chat gpt is aware of previous questions and answers

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 
@@ -25,13 +26,21 @@ prompt = ChatPromptTemplate(
             "or what columns exist.  Instead, use the 'describe_tables'"
             "function"
         )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         # Will look for an input with the name "agent_scratchpad".
         # the agent scratchpad is basically just memory that remembers the initial human message,
-        # any AI responses, function call results, etc... that it can pass during subsequant requests
+        # any AI responses, function call results, etc... that it can pass during subsequant requests.
+        # The problem here is that once we get a response from ChatGPT that is not a function call, it
+        # considers the job done and the agent_scratchpad intermediate steps get deleted.
+        # This means that if you ask another question, ChatGPT will not have any context of the previous
+        # stuff.
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
+
+# return_messages tells it to return the results as message objects instead of stringss
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
@@ -48,7 +57,13 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("Summarize the top 5 most popular products.  Write the results to a report file.")
+agent_executor(
+    "How many orders are there?  Write the results to a report file.")
+
+agent_executor(
+    "Repeat the exact same process for users"
+)
